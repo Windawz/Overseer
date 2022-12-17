794a8fd08a4fbfee9a81dd670ef3bbe878feef19
Windawz <46968683+Windawz@users.noreply.github.com>
2022-12-17 11:38:10 +0300

    Add entity types

---
+++ App/Data/Entities/Body.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Overseer.App.Data.Entities;
+
+public class Body {
+    public int Id { get; set; }
+    [AllowNull]
+    public string Name { get; set; }
+    [AllowNull]
+    public List<Service> Services { get; set; }
+}
