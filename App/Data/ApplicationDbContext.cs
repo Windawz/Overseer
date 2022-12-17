0bea41e3b3f6b6407df22535abadaede98445fbe
Windawz <46968683+Windawz@users.noreply.github.com>
2022-12-17 11:45:32 +0300

    Add db sets for entities

--- App/Data/ApplicationDbContext.cs
+++ App/Data/ApplicationDbContext.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
+using Overseer.App.Data.Entities;
 
 namespace Overseer.App.Data;
 public class ApplicationDbContext : IdentityDbContext {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
+
+    [AllowNull]
+    public DbSet<Service> Services { get; set; }
+    [AllowNull]
+    public DbSet<Bank> Banks { get; set; }
+    [AllowNull]
+    public DbSet<Body> Bodies { get; set; }
+    [AllowNull]
+    public DbSet<Tax> Taxes { get; set; }
+    [AllowNull]
+    public DbSet<Debt> Debts { get; set; }
+    [AllowNull]
+    public DbSet<RedemptionAttempt> RedemptionAttempts { get; set; }
+    [AllowNull]
+    public DbSet<RedemptionConclusion> RedemptionConclusions { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder) {
         base.OnModelCreating(builder);
