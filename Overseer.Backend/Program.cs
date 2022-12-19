04be802c35e00fbfeecabe18aba582ddb1875714
Windawz <46968683+Windawz@users.noreply.github.com>
2022-12-19 19:34:45 +0300

    Changes:
    Remove default example code;
    Add service and configuration boilerplate to Main;
    Scaffold a controller for banks

--- Overseer.Backend/Program.cs
+++ Overseer.Backend/Program.cs
@@ -6,37 +6,17 @@ public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddAuthorization();
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
             ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddControllers();
         builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        builder.Services.AddEndpointsApiExplorer();
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) => {
-            var forecast =  Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = summaries[Random.Shared.Next(summaries.Length)]
-                })
-                .ToArray();
-            return forecast;
-        });
-
+        app.MapControllers();
         app.Run();
     }
 }
