1641a5edb57fc0fee6f65af683df996eb38d71e5
Windawz <46968683+Windawz@users.noreply.github.com>
2022-12-19 12:42:56 +0300

    Delete App project folder

--- App/Data/Entities/Bank.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace Overseer.App.Data.Entities;
-
-public class Bank {
-    public int Id { get; set; }
-    [AllowNull]
-    public string Name { get; set; }
-}
--- App/Data/Entities/Tax.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Overseer.App.Data.Entities;
-
-public class Tax {
-    public int Id { get; set; }
-    public double Rate { get; set; }
-}
